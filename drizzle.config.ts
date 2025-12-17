d18776fa2e08209dfd852981c077049738df814d
themeetpatell <the.meetpatell@gmail.com>
2025-12-17 09:48:45 +0400

    fix: remove secrets and update config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,11 @@
 import type { Config } from 'drizzle-kit';
 
-const dbConfig: Config = {
+export default {
   schema: './src/db/schema.ts',
   out: './drizzle',
   driver: 'turso',
   dbCredentials: {
-    url: process.env.TURSO_CONNECTION_URL!,
-    authToken: process.env.TURSO_AUTH_TOKEN!,
+    url: process.env.TURSO_CONNECTION_URL || 'file:local.db',
+    authToken: process.env.TURSO_AUTH_TOKEN,
   },
-};
-
-export default dbConfig;
+} satisfies Config;
